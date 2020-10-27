86f71b48d485c118f3f24c442a606bc98bad9598
Gabriel Oliveira <oliveira.gahenrique@gmail.com>
2020-10-27 04:30:23 -0300

    implements tests

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -3,7 +3,73 @@ defmodule Helpers do
   def get_event_logs() do
     base_folder = "#{@env_folder}/nodes/#{node()}/data"
 
-    File.read!("#{base_folder}/events.log")
-    |> String.split("\n")
+    case File.read("#{base_folder}/events.log") do
+      {:ok, binary} ->
+        String.split(binary, "\n")
+
+      _ ->
+        []
+    end
+  end
+
+  defp inet_loader_args do
+    to_charlist("-loader inet -hosts 127.0.0.1 -setcookie #{:erlang.get_cookie()}")
+  end
+
+  defp rpc(node, module, function, args) do
+    :rpc.block_call(node, module, function, args)
+  end
+
+  defp add_code_paths(node) do
+    rpc(node, :code, :add_paths, [:code.get_path()])
+  end
+
+  defp transfer_configuration(node) do
+    for {app_name, _, _} <- Application.loaded_applications() do
+      for {key, val} <- Application.get_all_env(app_name) do
+        rpc(node, Application, :put_env, [app_name, key, val])
+      end
+    end
+  end
+
+  defp ensure_applications_started(node) do
+    rpc(node, Application, :ensure_all_started, [:mix])
+    rpc(node, Mix, :env, [Mix.env()])
+
+    for {app_name, _, _} <- Application.loaded_applications() do
+      rpc(node, Application, :ensure_all_started, [app_name])
+    end
+  end
+
+  def spawn_node(node_name) do
+    {:ok, node} =
+      :slave.start(to_charlist("127.0.0.1"), String.to_atom(node_name), inet_loader_args())
+
+    add_code_paths(node)
+    transfer_configuration(node)
+    ensure_applications_started(node)
+
+    connect_nodes(node)
+
+    node
+  end
+
+  def restart_processes do
+    Node.list([:this, :visible])
+    |> Enum.each(fn node ->
+      pid = rpc(node, Process, :whereis, [DistributedLogger])
+      rpc(node, Process, :exit, [pid, :kill])
+    end)
+
+    :ok
+  end
+
+  defp connect_nodes(node) do
+    Node.list([:this, :visible])
+    |> Enum.each(fn other_node ->
+      rpc(other_node, Node, :connect, [node])
+    end)
+
+    :ok
   end
 end
---
+++ test/distributed_logger_test.exs
@@ -0,0 +1,123 @@
+defmodule DistributedLoggerTest do
+  use ExUnit.Case
+
+  setup do
+    File.rm_rf(Application.get_env(:logger, :event_logs_env_folder))
+
+    Helpers.restart_processes()
+
+    :ok
+  end
+
+  setup_all %{} do
+    Helpers.spawn_node("node1")
+    Helpers.spawn_node("node2")
+    Helpers.spawn_node("node3")
+
+    on_exit(fn ->
+      Node.list()
+      |> Enum.each(&:slave.stop/1)
+    end)
+  end
+
+  defp write_on_node_local(node, data) do
+    :rpc.block_call(:"#{node}@127.0.0.1", DistributedLogger, :write_local, [data])
+  end
+
+  defp write_on_node_global(node, data) do
+    :rpc.block_call(:"#{node}@127.0.0.1", DistributedLogger, :write_global, [data])
+  end
+
+  defp read_events_node(node) do
+    :rpc.block_call(:"#{node}@127.0.0.1", Helpers, :get_event_logs, [])
+  end
+
+  defp handle_timestamp(result) do
+    {timestamp, data} =
+      String.split(result)
+      |> List.pop_at(0)
+
+    normalized_data =
+      data
+      |> Enum.reduce("", fn word, acc -> "#{acc} #{word}" end)
+      |> String.trim()
+
+    {timestamp, normalized_data}
+  end
+
+  test "should write events in order" do
+    write_on_node_local("node1", "first event")
+    write_on_node_local("node1", "second event")
+    write_on_node_local("node1", "third event")
+    write_on_node_local("node1", "fourth event")
+
+    [first, second, third, fourth, _] = read_events_node("node1")
+
+    assert first == "first event"
+    assert second == "second event"
+    assert third == "third event"
+    assert fourth == "fourth event"
+  end
+
+  test "should write only inside node" do
+    write_on_node_local("node1", "first event")
+    write_on_node_local("node3", "second event")
+
+    [first, _] = read_events_node("node1")
+    [second, _] = read_events_node("node3")
+
+    assert first == "first event"
+    assert second == "second event"
+
+    assert read_events_node("node2") == []
+  end
+
+  test "should write with timestamp on all nodes in order" do
+    write_on_node_global("node1", "first event")
+    write_on_node_global("node2", "second event")
+    write_on_node_global("node3", "third event")
+    write_on_node_global("node3", "fourth event")
+    write_on_node_global("node2", "fifth event")
+    write_on_node_global("node1", "sixth event")
+
+    [first1, second1, third1, fourth1, fifth1, sixth1, _] = read_events_node("node1")
+    [first2, second2, third2, fourth2, fifth2, sixth2, _] = read_events_node("node2")
+    [first3, second3, third3, fourth3, fifth3, sixth3, _] = read_events_node("node3")
+
+    assert {timestamp_first_1, "first event"} = handle_timestamp(first1)
+    assert {timestamp_first_2, "first event"} = handle_timestamp(first2)
+    assert {timestamp_first_3, "first event"} = handle_timestamp(first3)
+    assert timestamp_first_1 == timestamp_first_2
+    assert timestamp_first_1 == timestamp_first_3
+
+    assert {timestamp_second_1, "second event"} = handle_timestamp(second1)
+    assert {timestamp_second_2, "second event"} = handle_timestamp(second2)
+    assert {timestamp_second_3, "second event"} = handle_timestamp(second3)
+    assert timestamp_second_1 == timestamp_second_2
+    assert timestamp_second_1 == timestamp_second_3
+
+    assert {timestamp_third_1, "third event"} = handle_timestamp(third1)
+    assert {timestamp_third_2, "third event"} = handle_timestamp(third2)
+    assert {timestamp_third_3, "third event"} = handle_timestamp(third3)
+    assert timestamp_third_1 == timestamp_third_2
+    assert timestamp_third_1 == timestamp_third_3
+
+    assert {timestamp_fourth_1, "fourth event"} = handle_timestamp(fourth1)
+    assert {timestamp_fourth_2, "fourth event"} = handle_timestamp(fourth2)
+    assert {timestamp_fourth_3, "fourth event"} = handle_timestamp(fourth3)
+    assert timestamp_fourth_1 == timestamp_fourth_2
+    assert timestamp_fourth_1 == timestamp_fourth_3
+
+    assert {timestamp_fifth_1, "fifth event"} = handle_timestamp(fifth1)
+    assert {timestamp_fifth_2, "fifth event"} = handle_timestamp(fifth2)
+    assert {timestamp_fifth_3, "fifth event"} = handle_timestamp(fifth3)
+    assert timestamp_fifth_1 == timestamp_fifth_2
+    assert timestamp_fifth_1 == timestamp_fifth_3
+
+    assert {timestamp_sixth_1, "sixth event"} = handle_timestamp(sixth1)
+    assert {timestamp_sixth_2, "sixth event"} = handle_timestamp(sixth2)
+    assert {timestamp_sixth_3, "sixth event"} = handle_timestamp(sixth3)
+    assert timestamp_sixth_1 == timestamp_sixth_2
+    assert timestamp_sixth_1 == timestamp_sixth_3
+  end
+end
